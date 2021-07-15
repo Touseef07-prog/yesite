8c2fd561ff69873b522e0c72f7e2e9ce0d8f4508
Touseef Ahmad <58362982+Touseef07-prog@users.noreply.github.com>
2021-07-15 20:14:44 +0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,21 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
-from PIL import Image
+from django.urls import reverse
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+class post(models.Model):
+    title=models.CharField(max_length=64)
+    content=models.TextField()
+    date=models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User,on_delete=models.CASCADE, db_constraint=False)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.title
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- views.py
+++ views.py
@@ -1,42 +1,65 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+UserPassesTestMixin)
+from django.views.generic import (ListView,
+DetailView,
+CreateView,
+UpdateView,
+DeleteView)
+from .models import post
 
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'register.html', {'form': form})
-
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
+def index(request):
+    context ={
+    'posts':post.objects.all()
     }
 
-    return render(request, 'profile.html', context)
+    return render(request, 'index.html',context)
+
+class PostListView(ListView):
+    model = post
+    template_name = 'index.html'
+    context_object_name ='posts'
+    ordering =['-date']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = post
+    fields =['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = post
+    fields =['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = post
+    success_url ='/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+def about(request):
+    return render(request, 'about.html')
